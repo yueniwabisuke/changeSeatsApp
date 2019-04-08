094fcff175caa5850f8d79f2497c890866390b04
s.ishida <s.ishida-m@mediaeng.ics.kagoshima-u.ac.jp>
2019-04-08 18:13:01 +0900

    訂正した

---
+++ js/main.js
@@ -0,0 +1,29 @@
+$(function(){
+    var idNo = 0;
+    var person = localStorage.getItem("person");
+
+    for(i=0; i<person; i++){
+        $('div#templateForm')
+            // コピー処理
+            .clone(true)
+            // 不要なID削除
+            .removeAttr("id")
+            // テキストボックスのID追加
+            .find("input[name=templateTextbox]")
+            .attr("id", "textbox_" + idNo)
+            .end()
+            // 性別のID追加
+            .find("input[name=sex]")
+            .attr("id", "sex_" + idNo)
+            .attr("name","name_" + idNo)
+            .end()
+            // 追加処理
+            .appendTo("div#displayArea");
+        
+        // ID番号加算
+        idNo++;
+    }
+    //ひな型を非表示にする
+    $('div#templateForm.NotDisp')
+        .hide();
+});
